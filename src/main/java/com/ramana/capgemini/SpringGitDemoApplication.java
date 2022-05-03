0cf0d4420a69617946f4502f4911ef6bac45da77
ryearva <rramanareddy28@gmail.com>
2022-05-03 17:51:24 +0530

    Project innitial commits

---
+++ src/main/java/com/ramana/capgemini/SpringGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ramana.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramana/capgemini/SpringGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramana.capgemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
