d8c8e4d35c6f3d9ae6624023f64da617c427bcf8
ryearva <rramanareddy28@gmail.com>
2022-05-03 19:32:49 +0530

    aded cal class

---
+++ src/main/java/com/ramana/capgemini/entity/Car.java
@@ -0,0 +1,32 @@
+package com.ramana.capgemini.entity;
+
+public class Car {
+	private int carId;
+	private String carName;
+	private int price;
+
+	public int getCarId() {
+		return carId;
+	}
+
+	public void setCarId(int carId) {
+		this.carId = carId;
+	}
+
+	public String getCarName() {
+		return carName;
+	}
+
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+}
